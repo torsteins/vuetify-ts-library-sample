ee2bcc60a52dcbafe9bde3aacedce8a709baa680
Torstein Strømme <tot@vizrt.com>
2021-01-15 10:35:54 +0100

    Simplifying rollup config

--- abcd/build/rollup.config.js
+++ abcd/build/rollup.config.js
@@ -3,10 +3,9 @@ import fs from 'fs';
 import path from 'path';
 import vue from 'rollup-plugin-vue';
 import alias from '@rollup/plugin-alias';
-import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
 import replace from '@rollup/plugin-replace';
-import babel from 'rollup-plugin-babel';
-import { terser } from 'rollup-plugin-terser';
+import babel from '@rollup/plugin-babel';
 import minimist from 'minimist';
 
 // Get browserslist config and remove ie from es build targets
@@ -18,144 +17,60 @@ const esbrowserslist = fs.readFileSync('./.browserslistrc')
 const argv = minimist(process.argv.slice(2));
 
 const projectRoot = path.resolve(__dirname, '..');
+const extensions = ['.js', '.jsx', '.ts', '.tsx', '.vue'];
+const customResolver = resolve({extensions})
 
-const baseConfig = {
+// Customize configs for individual targets
+const esConfig = {
   input: 'src/entry.ts',
-  plugins: {
-    preVue: [
-      alias({
-        resolve: ['.js', '.jsx', '.ts', '.tsx', '.vue'],
-        entries: {
-          '@': path.resolve(projectRoot, 'src'),
-        },
-      }),
-    ],
-    replace: {
+  external: [
+    'vue',
+    'vue-property-decorator',
+    'vuetify'
+  ],
+  output: {
+    file: 'dist/abcd.esm.js',
+    format: 'esm',
+    exports: 'named',
+  },
+  plugins: [
+    replace({
       'process.env.NODE_ENV': JSON.stringify('production'),
-      'process.env.ES_BUILD': JSON.stringify('false'),
-    },
-    vue: {
+      'process.env.ES_BUILD': JSON.stringify('true'),
+    }),
+    alias({
+      customResolver,
+      entries: {
+        '@': path.resolve(projectRoot, 'src'),
+      },
+    }),
+    vue({
       css: true,
       template: {
         isProduction: true,
       },
-    },
-    babel: {
+    }),
+    babel({
       exclude: 'node_modules/**',
-      extensions: ['.js', '.jsx', '.ts', '.tsx', '.vue'],
-    },
-  },
-};
-
-// ESM/UMD/IIFE shared settings: externals
-// Refer to https://rollupjs.org/guide/en/#warning-treating-module-as-external-dependency
-const external = [
-  // list external dependencies, exactly the way it is written in the import statement.
-  // eg. 'jquery'
-  'vue',
-  'vue-property-decorator',
-  'vuetify'
-];
-
-// UMD/IIFE shared settings: output.globals
-// Refer to https://rollupjs.org/guide/en#output-globals for details
-const globals = {
-  // Provide global variable names to replace your external imports
-  // eg. jquery: '$'
-  vue: 'Vue',
-};
-
-// Customize configs for individual targets
-const buildFormats = [];
-if (!argv.format || argv.format === 'es') {
-  const esConfig = {
-    ...baseConfig,
-    external,
-    output: {
-      file: 'dist/abcd.esm.js',
-      format: 'esm',
-      exports: 'named',
-    },
-    plugins: [
-      replace({
-        ...baseConfig.plugins.replace,
-        'process.env.ES_BUILD': JSON.stringify('true'),
-      }),
-      ...baseConfig.plugins.preVue,
-      vue(baseConfig.plugins.vue),
-      babel({
-        ...baseConfig.plugins.babel,
-        presets: [
-          [
-            '@babel/preset-env',
-            {
-              targets: esbrowserslist,
-            },
-          ],
+      extensions,
+      presets: [
+        [
+          '@babel/preset-env',
+          {
+            targets: {
+              esmodules: true,
+            }
+          },
         ],
-      }),
-      commonjs(),
-    ],
-  };
-  buildFormats.push(esConfig);
-}
-
-if (!argv.format || argv.format === 'cjs') {
-  const umdConfig = {
-    ...baseConfig,
-    external,
-    output: {
-      compact: true,
-      file: 'dist/abcd.ssr.js',
-      format: 'cjs',
-      name: 'Abcd',
-      exports: 'named',
-      globals,
-    },
-    plugins: [
-      replace(baseConfig.plugins.replace),
-      ...baseConfig.plugins.preVue,
-      vue({
-        ...baseConfig.plugins.vue,
-        template: {
-          ...baseConfig.plugins.vue.template,
-          optimizeSSR: true,
-        },
-      }),
-      babel(baseConfig.plugins.babel),
-      commonjs(),
-    ],
-  };
-  buildFormats.push(umdConfig);
-}
-
-if (!argv.format || argv.format === 'iife') {
-  const unpkgConfig = {
-    ...baseConfig,
-    external,
-    output: {
-      compact: true,
-      file: 'dist/abcd.min.js',
-      format: 'iife',
-      name: 'Abcd',
-      exports: 'named',
-      globals,
-    },
-    plugins: [
-      replace(baseConfig.plugins.replace),
-      ...baseConfig.plugins.preVue,
-      vue(baseConfig.plugins.vue),
-      babel(baseConfig.plugins.babel),
-      commonjs(),
-      terser({
-        output: {
-          ecma: 5,
-        },
-      }),
-    ],
-  };
-  buildFormats.push(unpkgConfig);
-}
+        '@babel/preset-typescript'
+      ],
+      plugins: [
+        ["@babel/plugin-proposal-decorators", { "legacy": true }],
+        ["@babel/plugin-proposal-class-properties", { "loose": true}],
+      ]
+    }),
+  ],
+};
 
 // Export config
-export default buildFormats;
+export default esConfig;
